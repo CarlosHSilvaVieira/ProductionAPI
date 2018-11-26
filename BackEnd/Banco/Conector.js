eaa3235b250642035abd4cd614c8d2409a50159b
Carlos <carlos.h.silva.vieira@gmail.com>
2018-11-26 13:22:57 -0200

    Trying fix errors part 2

--- BackEnd/Banco/Conector.js
+++ BackEnd/Banco/Conector.js
@@ -1,8 +1,8 @@
 const Sequelize = require('sequelize');
 
-var sequelize = new Sequelize('INSERIR O NOME DO BANCO', 'INSERIR O USUARIO DO BANCO', 'INSERIR A SENHA DO USUARIO',
+var sequelize = new Sequelize('mydb', 'root', '123456',
     {
-        host: 'INSERIR A URL DO BANCO',
+        host: 'localhost',
         dialect: 'mysql',
         operatorsAliases: false,
         pool: {
